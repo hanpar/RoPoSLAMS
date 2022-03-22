9394718f16fdf751180f8fcf572aecc7115ff01f
vishrutskaushik <vishrutskaushik@gmail.com>
2022-03-22 14:12:47 -0400

    Working A-LOAM, floam for ROS Noetic in submodules

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/vishrut/ros_workspaces/eecs568-group17-project/src
+# Build directory: /home/vishrut/ros_workspaces/eecs568-group17-project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("A-LOAM")
+subdirs("floam")
---
+++ build/floam/CMakeFiles/floam_laser_processing_node.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "/home/vishrut/ros_workspaces/eecs568-group17-project/devel/lib/floam/floam_laser_processing_node"
+  "/home/vishrut/ros_workspaces/eecs568-group17-project/devel/lib/floam/floam_laser_processing_node.pdb"
+  "CMakeFiles/floam_laser_processing_node.dir/src/laserProcessingClass.cpp.o"
+  "CMakeFiles/floam_laser_processing_node.dir/src/laserProcessingNode.cpp.o"
+  "CMakeFiles/floam_laser_processing_node.dir/src/lidar.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/floam_laser_processing_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
