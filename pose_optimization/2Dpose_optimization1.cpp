51042fd3a4514b82b8fc3d9a2ac823ac87acd1b0
Yuehlint <yuehlint@umich.edu>
2022-04-09 17:06:23 -0400

    Delete y acc

--- pose_optimization/2Dpose_optimization1.cpp
+++ pose_optimization/2Dpose_optimization1.cpp
@@ -1,5 +1,6 @@
 #include "pose_optimization.h"
 #include </mnt/c/Users/adam8/Desktop/Umich/Course/ROB530/Project/eecs568-group17-project/reading_data/matplotlib-cpp/matplotlibcpp.h>
+#include <cmath>
 
 using namespace std; 
 using namespace gtsam; 
@@ -13,69 +14,60 @@ void runBatch(vector<VECTOR_SE2> slamPoses, vector<EDGE_SE2> imuMeasurements){
     
     // Add prior
     noiseModel::Diagonal::shared_ptr priorNoise = noiseModel::Diagonal::Variances(Vector3(1e-6,1e-6,1e-8)); 
-    graph.add(PriorFactor<Pose2>(1, Pose2(1, 1, 0.1), priorNoise));
-    
+    graph.add(PriorFactor<Pose2>(slamPoses.at(0).idx, Pose2(slamPoses.at(0).x,slamPoses.at(0).y,slamPoses.at(0).theta), priorNoise));
+    int limit = 20;
+    //int limit = imuMeasurements.size();
     // Add vertices and edges
-    int limit = imuMeasurements.size();
-    cout << "Imu measurement = " << limit;
-    cout << "SLAM measurement = " << slamPoses.size() << endl;
-    
-    //int limit = 100;
-    for (int i = 2; i < limit - 1; i++){ 
+    for (int i = 1; i < limit - 1; i++){ 
     	EDGE_SE2 tempEdge = imuMeasurements.at(i);
-    	noiseModel::Gaussian::shared_ptr model = noiseModel::Diagonal::Variances(Vector3(2e-8,2e-8,2e-8)); 
+    	noiseModel::Gaussian::shared_ptr model = noiseModel::Diagonal::Variances(Vector3(1e-6,1e-6,1e-8)); 
     	graph.add(BetweenFactor<Pose2>(tempEdge.idx-1,tempEdge.idx, Pose2(tempEdge.dx,tempEdge.dy,tempEdge.dtheta), model)); 
     	
     }
-    cout << "EDGES added " << endl;
+    cout << "EDGES added" << endl;
     vector<double> floam_x, floam_y;
-    limit = slamPoses.size();
-    // limit = 50;
-    for(int i = 1; i < limit; i++){
+    for(int i = 0; i < limit; i++){
     	auto tempPose = slamPoses.at(i);
-    	initial.insert(i, Pose2(tempPose.x,tempPose.y,tempPose.theta));
+    	initial.insert(i,Pose2(tempPose.x,tempPose.y,tempPose.theta));
         floam_x.push_back(tempPose.x);
         floam_y.push_back(tempPose.y);
     }
-    cout << "Poses added " << endl;
+    cout << "Poses added" << endl;
     GaussNewtonParams parameters; 
-    parameters.relativeErrorTol = 1e-5; 
-    parameters.maxIterations = 10000; 
+    parameters.relativeErrorTol = 1e-2; 
+    parameters.maxIterations = 100; 
     GaussNewtonOptimizer optimizer(graph,initial,parameters);
-    // LevenbergMarquardtOptimizer optimizer(graph, initial);
     
     Values result = optimizer.optimize();
-    //result.print(); 
-    vector<double> post_x, post_y;
-    //   Save results to file
+    // result.print(); 
+
+      // Save results to file
+    cout << "finish" << endl;
     printf("\nWriting results to file...\n");
-    string output_filename = "optimized_poses_without_imu.txt";
+    string output_filename = "optimized_poses.txt";
     FILE* fp_out = fopen(output_filename.c_str(), "w+");
     fprintf(fp_out,
             "#time(s),x(m),y(m),theta(m)\n");
-
-    for (size_t i = 1; i < limit - 1; i++) {
+    vector<double> post_x, post_y;
+    for (size_t i = 0; i < limit - 1; i++) {
 
         auto pose = result.at<Pose2>(i);
 
-        //cout << "State at #" << i << endl;
-        //cout << "Pose:" << endl << pose << endl;
+        cout << "State at #" << i << endl;
+        cout << "Pose:" << endl << pose << endl;
 
         fprintf(fp_out, "%lld, %f,%f,%f\n",
                 slamPoses[i].time, pose.x(), pose.y(), pose.theta());
         post_x.push_back(pose.x());
         post_y.push_back(pose.y());
     }
-    //cout << "slamePose size" << slamPoses.size() << endl;
-
     fclose(fp_out);
     plt::figure(1);
-    plt::plot(post_x,post_y,"-s");
-    plt::title("Post Process");
-    plt::figure(2);
-    plt::plot(floam_x,floam_y,"-s");
-    plt::title("Floam Initial");
-    plt::show();
+    plt::plot(floam_x,floam_y);
+    plt::plot(post_x,post_y);
+    plt::title("result");
+    //plt::save("Initial_F-LOAN_Point.png");
+    
 }
 
 void runISAM(vector<VECTOR_SE2> slamPoses, vector<EDGE_SE2> imuMeasurements){
@@ -88,11 +80,11 @@ void runISAM(vector<VECTOR_SE2> slamPoses, vector<EDGE_SE2> imuMeasurements){
     
     NonlinearFactorGraph graph;
     // Add vertices and edges
-    for (int i = 0; i < slamPoses.size() - 1; i++){
+    for (int i = 0; i < slamPoses.size(); i++){
     	graph.resize(0); 
     	Values initial; 
     	if (i == 0){
-    	noiseModel::Diagonal::shared_ptr priorNoise = noiseModel::Diagonal::Variances(Vector3(1e-8,1e-8,1e-8)); 
+    	noiseModel::Diagonal::shared_ptr priorNoise = noiseModel::Diagonal::Variances(Vector3(1e-6,1e-6,1e-8)); 
     	graph.add(PriorFactor<Pose2>(slamPoses.at(0).idx, Pose2(slamPoses.at(0).x,slamPoses.at(0).y,slamPoses.at(0).theta), priorNoise));
     		initial.insert(slamPoses.at(0).idx, Pose2(slamPoses.at(0).x,slamPoses.at(0).y,slamPoses.at(0).theta));
     	}
@@ -103,7 +95,7 @@ void runISAM(vector<VECTOR_SE2> slamPoses, vector<EDGE_SE2> imuMeasurements){
     	   	EDGE_SE2 tempEdge = imuMeasurements.at(i);
             
     	   	// if (i==tempEdge.idx+1){
-	    	noiseModel::Gaussian::shared_ptr model = noiseModel::Gaussian::Covariance(Vector3(10,10,0.001));
+	    	noiseModel::Gaussian::shared_ptr model = noiseModel::Gaussian::Covariance(Vector3(0.05,0.05,0.05));
 	    	graph.add(BetweenFactor<Pose2>(i-1, i, Pose2(tempEdge.dx,tempEdge.dy,tempEdge.dtheta), model)); 
     	   	// }
     	//    }
@@ -111,14 +103,14 @@ void runISAM(vector<VECTOR_SE2> slamPoses, vector<EDGE_SE2> imuMeasurements){
     	isam.update(graph,initial); 
     	result = isam.calculateEstimate(); 
     }
-    //result.print(); 
+    result.print(); 
 }
 
 void integrateIMUData(vector<EDGE_SE3> &imuMeasurements_SE3, vector<EDGE_SE2> &imuMeasurements){
 
-    double prev_vel_x = 5;
+    double prev_vel_x = 0;
     double prev_vel_y = 0;
-    double prev_angle = 1.8;
+    double prev_angle = 0;
     double current_angle = 0;
     double current_vel_x = 0;
     double current_vel_y = 0;
@@ -134,39 +126,43 @@ void integrateIMUData(vector<EDGE_SE3> &imuMeasurements_SE3, vector<EDGE_SE2> &i
     vector<double> IMU_x, IMU_y;
     IMU_x.push_back(corr_x);
     IMU_y.push_back(corr_y);
-    int limit = imuMeasurements_SE3.size() ;
-    //int limit = 200;
-    for(int i = 1; i < limit; i++){
+        
+    for(int i = 1; i < imuMeasurements_SE3.size() - 1; i++){
         dt = (imuMeasurements_SE3.at(i).time - imuMeasurements_SE3.at(i-1).time) * pow(10, -9);
         current_angle =  (double) atan2(2 * (imuMeasurements_SE3.at(i).q.w() * imuMeasurements_SE3.at(i).q.z() + imuMeasurements_SE3.at(i).q.y() * imuMeasurements_SE3.at(i).q.x()), 1 - 2 * (pow(imuMeasurements_SE3.at(i).q.x(), 2) + pow(imuMeasurements_SE3.at(i).q.z(), 2)));
+
         current_vel_x = prev_vel_x + imuMeasurements_SE3.at(i).accel(0) * dt;
-        //current_vel_y = prev_vel_y + imuMeasurements_SE3.at(i).accel(1) * dt;
+        current_vel_y = prev_vel_y + imuMeasurements_SE3.at(i).accel(1) * dt;
 
-        imuMeasurement.dx = current_vel_x * dt;// * cos(current_angle); // - current_vel_y * dt * sin(current_angle);
-        imuMeasurement.dy = 0;//current_vel_x * dt// * sin(current_angle); //+ current_vel_y * dt * cos(current_angle);
+        imuMeasurement.dx = current_vel_x * dt * cos(current_angle) - current_vel_y * dt * sin(current_angle);
+        imuMeasurement.dy = current_vel_x * dt * sin(current_angle) + current_vel_y * dt * cos(current_angle);
+
         
-
-        imuMeasurement.dtheta =  current_angle - prev_angle;//(double) atan2(2 * (imuMeasurements_SE3.at(i).q.w() * imuMeasurements_SE3.at(i).q.z() + imuMeasurements_SE3.at(i).q.y() * imuMeasurements_SE3.at(i).q.x()), 1 - 2 * (pow(imuMeasurements_SE3.at(i).q.x(), 2) + pow(imuMeasurements_SE3.at(i).q.z(), 2))) * dt;
+        if (i == 1) {
+            imuMeasurement.dtheta = 0;
+        } else { 
+            imuMeasurement.dtheta = current_angle - prev_angle;
+        } 
         imuMeasurement.time = imuMeasurements_SE3.at(i).time;
         imuMeasurement.idx = imuMeasurements_SE3.at(i).idx;
-
-        // cout << "dx = " << imuMeasurement.dx << " dy = " << imuMeasurement.dy << " dtheta = " << imuMeasurement.dtheta <<endl;
 
         imuMeasurements.push_back(imuMeasurement);
         prev_vel_x = current_vel_x;
         prev_vel_y = current_vel_y;
         prev_angle = current_angle;
+
         fprintf(fp_out, "%d, %f, %f, %f, %f\n",
                     imuMeasurement.idx, dt, imuMeasurement.dx, imuMeasurement.dy, imuMeasurement.dtheta);
+
         corr_x = corr_x + imuMeasurement.dx;
         corr_y = corr_y + imuMeasurement.dy;
         IMU_x.push_back(corr_x);
         IMU_y.push_back(corr_y);
     }
-    cout << "IMU Mesurement size" << imuMeasurements_SE3.size() << endl;
-    plt::figure(3);
-    plt::plot(IMU_x,IMU_y,"-s");
+    plt::figure(2);
+    plt::plot(IMU_x,IMU_y);
     plt::title("IMU Trajectory");
+    plt::show();
 }
 
 int main(const int argc, const char *argv[]) {
@@ -199,5 +195,6 @@ int main(const int argc, const char *argv[]) {
     integrateIMUData(imuMeasurements_SE3, imuMeasurements);
     cout << "Inetegrated IMU" <<endl;
     runBatch(slamPoses, imuMeasurements); 
+    plt::show();
 }
     
---
+++ pose_optimization/plot.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include </mnt/c/Users/adam8/Desktop/Umich/Course/ROB530/Project/eecs568-group17-project/reading_data/matplotlib-cpp/matplotlibcpp.h>
+
+using namespace std;
+
+namespace plt = matplotlibcpp;
+
+int main(const int argc, const char *argv[]) {
+    string poses_p_data = "./data/optimized_poses_without_imu.txt";
+       
+    string tf_data = "./data/optimized_poses.txt";
+
+    vector<double> x_wo_IMU, y_wo_IMU, theta_wo_IMU;
+    vector<double> x_w_IMU, y_w_IMU, theta_w_IMU;
+    vector<long long int> t,t1;
+    string line, line1;
+    
+    ifstream fin(poses_p_data);
+    getline(fin, line);  // ignore the first line
+    
+    long long int time;
+    double x,y,theta;
+    while ((fin >> time) && (fin >> x) && (fin >> y) && (fin >> theta))
+    {   
+        cout << time << endl;
+        t.push_back(time);
+        x_wo_IMU.push_back(x);
+        y_wo_IMU.push_back(y);
+        theta_wo_IMU.push_back(theta);
+    }
+    fin.close();
+    cout << x_wo_IMU.at(0) << endl; 
+    ifstream fin1(tf_data);
+    getline(fin1, line1);  // ignore the first line
+    
+    while (fin1 >> time >> x >> y >> theta)
+    {
+        t1.push_back(time);
+        x_w_IMU.push_back(x);
+        y_w_IMU.push_back(y);
+        theta_w_IMU.push_back(theta);
+        
+    }
+    cout << x_w_IMU.at(0) << endl;
+
+    plt::plot(x_wo_IMU,y_wo_IMU);
+    plt::plot(x_w_IMU,y_w_IMU);
+    plt::title("Comparison");
+    plt::show();
+}
+  
