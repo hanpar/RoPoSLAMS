47af4daf7beca7a2714ef418957e45b08ebbd4ed
Yuehlint <yuehlint@umich.edu>
2022-04-10 14:31:30 -0400

    2D Pose

---
+++ reading_data/CMakeFiles/read_data.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/read_data.dir/data_read.cpp.o"
+  "read_data"
+  "read_data.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/read_data.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
