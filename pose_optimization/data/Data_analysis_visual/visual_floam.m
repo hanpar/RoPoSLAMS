bd008486f1a7b1d49fd854b34e51fcae9f961189
Yuehlint <78129006+Yuehlint@users.noreply.github.com>
2022-04-20 21:34:01 -0400

    Match the initial value

--- pose_optimization/data/Data_analysis_visual/visual_floam.m
+++ pose_optimization/data/Data_analysis_visual/visual_floam.m
@@ -1,12 +1,12 @@
 clearvars
 
-gtkitti20110930drive0018 = importfile3("../floam_gt_kitti_2011_09_30_drive_notsync.txt", [2, Inf]);
-gtkitti_t = importfile4("../floam_gt_kitti_2011_09_30_drive_notsync.txt", [2, Inf]);
+gtkitti20110930drive0018 = importfile3("../floam_gt_kitti_2011_09_30_drive_notsync.txt", [4, Inf]);
+gtkitti_t = importfile4("../floam_gt_kitti_2011_09_30_drive_notsync.txt", [4, Inf]);
 optpose = importfile1("../../optimized_poses_with_imu_kitti_0018_notsync.txt", [2, Inf]);
-optpose_floam = importfile1("../../optimized_poses_without_imu_kitti_0018_notsync.txt", [2, Inf]);
+optpose_floam = importfile1("../../optimized_poses_without_imu_kitti_0018_notsync.txt", [6, Inf]);
 
 origin = [gtkitti20110930drive0018(1,1), gtkitti20110930drive0018(1,2)]
-
+len = 2523.6;
 % sync_time
 gpskitti_t = gtkitti_t - gtkitti_t(1);
 gpskitti_t = gpskitti_t/10^9;
@@ -23,19 +23,17 @@ error_fusiony = gtkitti20110930drive0018(:,2) - interp1(t_post,optpose(:,3),gpsk
 error_floamy = gtkitti20110930drive0018(:,2) - interp1(t_floam,optpose_floam(:,3),gpskitti_t,'linear');
 
 
-meanerror_fusionx = mean(abs(error_fusionx),'omitnan')
-meanerror_floamx = mean(abs(error_floamx),'omitnan')
+meanerror_fusionx = mean(abs(error_fusionx),'omitnan')/len
+meanerror_floamx = mean(abs(error_floamx),'omitnan')/len
 
-meanerror_fusiony = mean(abs(error_fusiony),'omitnan')
-meanerror_floamy = mean(abs(error_floamy),'omitnan')
+meanerror_fusiony = mean(abs(error_fusiony),'omitnan')/len
+meanerror_floamy = mean(abs(error_floamy),'omitnan')/len
 
-stdfx = std(error_fusionx,'omitnan')
-stdfy = std(error_fusiony,'omitnan')
+stdfx = std(error_fusionx,'omitnan')/len
+stdfy = std(error_fusiony,'omitnan')/len
 
-stdfloamx = std(error_floamx,'omitnan')
-stdfloamy = std(error_floamy,'omitnan')
-
-
+stdfloamx = std(error_floamx,'omitnan')/len
+stdfloamy = std(error_floamy,'omitnan')/len
 
 % 
 figure
